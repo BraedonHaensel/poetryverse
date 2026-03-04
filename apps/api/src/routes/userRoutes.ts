f26477340b2b536dbe6ec2caacbe395ddf9d7c09
Carson May <carson.may@ucalgary.ca>
2026-03-03 22:09:47 -0700

    fix: fix get users route

--- apps/api/src/routes/userRoutes.ts
+++ apps/api/src/routes/userRoutes.ts
@@ -3,7 +3,7 @@ import { prisma } from '../lib/db';
 
 const router = Router()
 
-router.get('/api/users', async (_req, res) => {
+router.get('/', async (_req, res) => {
     const users = await prisma.user.findMany();
     res.json(users);
 });
