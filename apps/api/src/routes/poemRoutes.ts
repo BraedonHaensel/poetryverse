977d63e0aea2f363b8af2d3e345d70206cbcdf4a
Rohan KAPILA <rohan.kapila@ucalgary.ca>
2026-03-06 17:27:27 -0700

    feat: import gemini-api and create route

--- apps/api/src/routes/poemRoutes.ts
+++ apps/api/src/routes/poemRoutes.ts
@@ -1,6 +1,12 @@
+import dotenv from "dotenv";
+dotenv.config();
+
 import { Router } from 'express'
+import { Request,Response } from 'express'
+import { GoogleGenerativeAI } from '@google/generative-ai'
 
 const router = Router()
+
 
 // An example:
 // router.get('/', getPoems);
