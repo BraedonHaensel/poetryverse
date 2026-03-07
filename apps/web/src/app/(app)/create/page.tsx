79f18fec5913841010a2d6e2ebbb986ecd4fc1ba
BraedonHaensel <bhaensel111@gmail.com>
2026-03-07 13:23:21 -0700

    style(create-page): centered card page layout

--- apps/web/src/app/(app)/create/page.tsx
+++ apps/web/src/app/(app)/create/page.tsx
@@ -23,6 +23,7 @@ import {
   SelectValue,
 } from '@/components/ui/select'
 import { Textarea } from '@/components/ui/textarea'
+import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card'
 
 // TODO Hardcoded example, clean up when we can get the poem types from the backend
 const POEM_TYPES = ['Haiku', 'Couplet', 'Sonnet']
@@ -47,128 +48,137 @@ export default function Create() {
   }
 
   return (
-    <Form {...form}>
-      <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-5">
-        {/* Poem type field */}
-        <FormField
-          control={form.control}
-          name="type"
-          render={({ field }) => (
-            <FormItem>
-              <FormLabel>Type</FormLabel>
-              <FormControl>
-                <Select value={field.value} onValueChange={field.onChange}>
-                  <SelectTrigger>
-                    <SelectValue placeholder="Select a poem type..." />
-                  </SelectTrigger>
-                  <SelectContent>
-                    {POEM_TYPES.map((type) => (
-                      <SelectItem key={type} value={type}>
-                        {type}
-                      </SelectItem>
-                    ))}
-                  </SelectContent>
-                </Select>
-              </FormControl>
-              <FormMessage />
-            </FormItem>
-          )}
-        />
+    <Card>
+      <CardHeader>
+        <CardTitle className="text-center font-bold">
+          Create From Scratch (TODO icon)
+        </CardTitle>
+      </CardHeader>
+      <CardContent>
+        <Form {...form}>
+          <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-5">
+            {/* Poem type field */}
+            <FormField
+              control={form.control}
+              name="type"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Type</FormLabel>
+                  <FormControl>
+                    <Select value={field.value} onValueChange={field.onChange}>
+                      <SelectTrigger>
+                        <SelectValue placeholder="Select a poem type..." />
+                      </SelectTrigger>
+                      <SelectContent>
+                        {POEM_TYPES.map((type) => (
+                          <SelectItem key={type} value={type}>
+                            {type}
+                          </SelectItem>
+                        ))}
+                      </SelectContent>
+                    </Select>
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
 
-        {/* Your Poem field */}
-        <FormField
-          control={form.control}
-          name="poem"
-          render={({ field }) => (
-            <FormItem>
-              <FormLabel>Your Poem</FormLabel>
-              <FormControl className="h-50">
-                <Textarea
-                  className="resize-none"
-                  placeholder="Write your poem..."
-                  maxLength={1000}
-                  {...field}
-                />
-              </FormControl>
-              <div className="flex w-full justify-between">
-                <FormMessage />
-                <div className="text-muted-foreground ml-auto text-right text-sm">
-                  {field.value?.length ?? 0}/{1000}
-                </div>
-              </div>
-            </FormItem>
-          )}
-        />
+            {/* Your Poem field */}
+            <FormField
+              control={form.control}
+              name="poem"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Your Poem</FormLabel>
+                  <FormControl className="h-50">
+                    <Textarea
+                      className="resize-none"
+                      placeholder="Write your poem..."
+                      maxLength={1000}
+                      {...field}
+                    />
+                  </FormControl>
+                  <div className="flex w-full justify-between">
+                    <FormMessage />
+                    <div className="text-muted-foreground ml-auto text-right text-sm">
+                      {field.value?.length ?? 0}/{1000}
+                    </div>
+                  </div>
+                </FormItem>
+              )}
+            />
 
-        {/* Poem title field */}
-        <FormField
-          control={form.control}
-          name="title"
-          render={({ field }) => (
-            <FormItem>
-              <FormLabel>Title</FormLabel>
-              <FormControl>
-                <Input placeholder="Title your poem..." {...field} />
-              </FormControl>
-              <FormMessage />
-            </FormItem>
-          )}
-        />
+            {/* Poem title field */}
+            <FormField
+              control={form.control}
+              name="title"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Title</FormLabel>
+                  <FormControl>
+                    <Input placeholder="Title your poem..." {...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
 
-        {/* Public Visibility field */}
-        <FormField
-          control={form.control}
-          name="publicVisibility"
-          render={({ field }) => (
-            <FormItem className="flex items-center justify-between">
-              <div>
-                <FormLabel>Public Visibility</FormLabel>
-                <FormDescription>
-                  Share your poem with the public.
-                </FormDescription>
-              </div>
-              <FormControl>
-                <Switch
-                  className="scale-150"
-                  checked={field.value}
-                  onCheckedChange={field.onChange}
-                />
-              </FormControl>
+            {/* Public Visibility field */}
+            <FormField
+              control={form.control}
+              name="publicVisibility"
+              render={({ field }) => (
+                <FormItem className="flex items-center justify-between">
+                  <div>
+                    <FormLabel>Public Visibility</FormLabel>
+                    <FormDescription>
+                      Share your poem with the public.
+                    </FormDescription>
+                  </div>
+                  <FormControl>
+                    <Switch
+                      className="scale-150"
+                      checked={field.value}
+                      onCheckedChange={field.onChange}
+                    />
+                  </FormControl>
 
-              <FormMessage />
-            </FormItem>
-          )}
-        />
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
 
-        {/* Created With AI Assistance field */}
-        <FormField
-          control={form.control}
-          name="createdWithAI"
-          render={({ field }) => (
-            <FormItem className="flex items-center justify-between">
-              <div>
-                <FormLabel>Created With AI Assistance</FormLabel>
-                <FormDescription>
-                  Uphold transparency by admitting whether AI was used in the
-                  creation of your poem.
-                </FormDescription>
-              </div>
-              <FormControl>
-                <Switch
-                  className="scale-150"
-                  checked={field.value}
-                  onCheckedChange={field.onChange}
-                />
-              </FormControl>
+            {/* Created With AI Assistance field */}
+            <FormField
+              control={form.control}
+              name="createdWithAI"
+              render={({ field }) => (
+                <FormItem className="flex items-center justify-between">
+                  <div>
+                    <FormLabel>Created With AI Assistance</FormLabel>
+                    <FormDescription>
+                      Uphold transparency by admitting whether AI was used in
+                      the creation of your poem.
+                    </FormDescription>
+                  </div>
+                  <FormControl>
+                    <Switch
+                      className="scale-150"
+                      checked={field.value}
+                      onCheckedChange={field.onChange}
+                    />
+                  </FormControl>
 
-              <FormMessage />
-            </FormItem>
-          )}
-        />
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
 
-        {/* Publish button */}
-        <Button type="submit">Publish</Button>
-      </form>
-    </Form>
+            {/* Publish button */}
+            <Button type="submit">Publish</Button>
+          </form>
+        </Form>
+      </CardContent>
+    </Card>
   )
 }
