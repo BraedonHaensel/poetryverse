03720a8ab6515b40d83c497d140738afed51e8fa
BraedonHaensel <bhaensel111@gmail.com>
2026-03-07 20:16:01 -0700

    feature(create-page): add tags selector

--- apps/web/src/app/(app)/create/fields/poem-tags-field.tsx
+++ apps/web/src/app/(app)/create/fields/poem-tags-field.tsx
@@ -1,13 +1,30 @@
-import { Control } from 'react-hook-form'
+'use client'
+
+import { Control, useController } from 'react-hook-form'
 import {
   FormControl,
-  FormField,
   FormItem,
   FormLabel,
   FormMessage,
 } from '@/components/ui/form'
 import { ShadowCard } from '@/components/shadow-card'
 import { CreateFromScratchSchema } from '@/schemas/createPoemSchemas'
+import {
+  Combobox,
+  ComboboxChip,
+  ComboboxChips,
+  ComboboxChipsInput,
+  ComboboxContent,
+  ComboboxEmpty,
+  ComboboxItem,
+  ComboboxList,
+  ComboboxValue,
+  useComboboxAnchor,
+} from '@/components/ui/combobox'
+import { X } from 'lucide-react'
+
+// TODO Hardcoded example, clean up when we can get the poem tags from the backend
+const POEM_TAGS = ['Nature', 'Romance', 'Comedy', 'Parody']
 
 type Props = {
   control: Control<CreateFromScratchSchema>
@@ -17,21 +34,70 @@ type Props = {
  * Poem tags field.
  */
 export function PoemTagsField({ control }: Props) {
+  const anchor = useComboboxAnchor()
+
+  const { field, fieldState } = useController({
+    control,
+    name: 'tags',
+  })
+
   return (
     <ShadowCard className="p-3">
-      <FormField
-        control={control}
-        name="tags"
-        render={({ field }) => (
-          <FormItem>
-            <FormLabel>Title</FormLabel>
-            <FormControl>
-              <p>TODO Add Tags</p>
-            </FormControl>
-            <FormMessage />
-          </FormItem>
-        )}
-      />
+      <FormItem>
+        <FormLabel>Type</FormLabel>
+        <FormControl>
+          <Combobox
+            multiple
+            autoHighlight
+            items={POEM_TAGS}
+            value={field.value}
+            onValueChange={field.onChange}
+          >
+            <ComboboxChips
+              ref={anchor}
+              // Override the default padding to fit the clear button
+              className="bg-off-white relative py-2! pr-8! pl-3!"
+            >
+              <ComboboxValue>
+                {field.value.map((tag) => (
+                  <ComboboxChip key={tag} className="bg-gray-300 text-sm">
+                    {tag}
+                  </ComboboxChip>
+                ))}
+              </ComboboxValue>
+              <ComboboxChipsInput
+                className="h-6"
+                placeholder={
+                  field.value.length === 0 ? 'Select poem tags...' : ''
+                }
+              />
+              {/* Clear button */}
+              {field.value.length > 0 && (
+                <X
+                  className="text-muted-foreground hover:text-foreground absolute top-1/2 right-2 -translate-y-1/2"
+                  size={18}
+                  onClick={() => field.onChange([])}
+                />
+              )}
+            </ComboboxChips>
+            <ComboboxContent anchor={anchor} className="bg-off-white">
+              <ComboboxEmpty>No tags found.</ComboboxEmpty>
+              <ComboboxList>
+                {(tag) => (
+                  <ComboboxItem
+                    key={tag}
+                    className="data-highlighted:bg-gray-200"
+                    value={tag}
+                  >
+                    {tag}
+                  </ComboboxItem>
+                )}
+              </ComboboxList>
+            </ComboboxContent>
+          </Combobox>
+        </FormControl>
+        <FormMessage>{fieldState?.error?.message}</FormMessage>
+      </FormItem>
     </ShadowCard>
   )
 }
--- apps/web/src/schemas/createPoemSchemas.ts
+++ apps/web/src/schemas/createPoemSchemas.ts
@@ -1,14 +1,16 @@
 import * as z from 'zod'
 
-// Input character limits
+// Validation limits
 const TITLE_MIN = 3
 const TITLE_MAX = 30
 const POEM_MIN = 20
 const POEM_MAX = 1000
+const MIN_TAGS = 1
+const MAX_TAGS = 5
 
 // Schema for validating the create poem from scratch form
 export const CreateFromScratchSchema = z.object({
-  type: z.string().min(1, 'Please select a poem type.'),
+  type: z.string().nonempty('Please select a poem type.'),
   poem: z
     .string()
     .min(POEM_MIN, `Poem must be at least ${POEM_MIN} characters.`)
@@ -17,7 +19,10 @@ export const CreateFromScratchSchema = z.object({
     .string()
     .min(TITLE_MIN, `Title must be at least ${TITLE_MIN} characters.`)
     .max(TITLE_MAX, `Poem must be at most ${TITLE_MAX} characters.`),
-  tags: z.array(z.string()).max(5, 'Poem can have at most 5 tags'),
+  tags: z
+    .array(z.string())
+    .min(MIN_TAGS, `Poem must have at least ${MIN_TAGS} tags.`)
+    .max(MAX_TAGS, `Poem can have at most ${MAX_TAGS} tags`),
   publicVisibility: z.boolean(),
   createdWithAI: z.boolean(),
 })
