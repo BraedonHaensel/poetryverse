7c113c9806a8971554e661d3758239bd8aa9d216
BraedonHaensel <bhaensel111@gmail.com>
2026-03-10 16:06:08 -0600

    feat(settings): add sign out button on mobile

--- apps/web/src/app/(app)/settings/page.tsx
+++ apps/web/src/app/(app)/settings/page.tsx
@@ -1,3 +1,6 @@
+'use client'
+
+import SignOutButton from '@/components/auth-buttons/sign-out-button'
 import { ShadowCard } from '@/components/shadow-card'
 import { CardContent, CardHeader, CardTitle } from '@/components/ui/card'
 
@@ -16,17 +19,20 @@ const EMAIL = 'myemail@email.com'
  */
 export default function UserSettings() {
   return (
-    <ShadowCard className="mx-auto w-full max-w-170">
+    <ShadowCard className="mx-auto h-190 w-full max-w-170 sm:h-auto">
+      {/* TODO when mobile layout is suppored, remove card and use h-full */}
       <CardHeader>
         <CardTitle className="mx-auto text-2xl font-bold">
           User Settings
         </CardTitle>
       </CardHeader>
-      <CardContent className="flex flex-col gap-5">
+      <CardContent className="flex h-full flex-col gap-5">
         <ProfilePictureForm imageUrl={IMAGE_URL} />
         <UsernameForm username={USERNAME} />
         <EmailForm email={EMAIL} />
         <AdvancedSettingsForm />
+        {/* Mobile-only sign out button */}
+        <SignOutButton className="mt-auto sm:hidden" text="Sign Out" />
       </CardContent>
     </ShadowCard>
   )
