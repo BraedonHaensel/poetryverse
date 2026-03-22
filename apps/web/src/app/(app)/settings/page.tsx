3b55f51a8e4d8ee8529da4090eec12e2a95b3921
BraedonHaensel <bhaensel111@gmail.com>
2026-03-21 21:52:51 -0600

    style: update pages for the new navbar layout

--- apps/web/src/app/(app)/settings/page.tsx
+++ apps/web/src/app/(app)/settings/page.tsx
@@ -19,21 +19,23 @@ const EMAIL = 'myemail@email.com'
  */
 export default function UserSettings() {
   return (
-    <ShadowCard className="mx-auto h-190 w-full max-w-170 sm:h-auto">
-      {/* TODO when mobile layout is suppored, remove card and use h-full */}
-      <CardHeader>
-        <CardTitle className="mx-auto text-2xl font-bold">
-          User Settings
-        </CardTitle>
-      </CardHeader>
-      <CardContent className="flex h-full flex-col gap-5">
-        <ProfilePictureForm imageUrl={IMAGE_URL} />
-        <UsernameForm username={USERNAME} />
-        <EmailForm email={EMAIL} />
-        <AdvancedSettingsForm />
-        {/* Mobile-only sign out button */}
-        <SignOutButton className="mt-auto sm:hidden" text="Sign Out" />
-      </CardContent>
-    </ShadowCard>
+    <div className="flex h-full min-h-fit p-10">
+      <ShadowCard className="m-auto h-190 w-full max-w-170 sm:h-auto">
+        {/* TODO when mobile layout is suppored, remove card and use h-full */}
+        <CardHeader>
+          <CardTitle className="mx-auto text-2xl font-bold">
+            User Settings
+          </CardTitle>
+        </CardHeader>
+        <CardContent className="flex h-full flex-col gap-5">
+          <ProfilePictureForm imageUrl={IMAGE_URL} />
+          <UsernameForm username={USERNAME} />
+          <EmailForm email={EMAIL} />
+          <AdvancedSettingsForm />
+          {/* Mobile-only sign out button */}
+          <SignOutButton className="mt-auto sm:hidden" text="Sign Out" />
+        </CardContent>
+      </ShadowCard>
+    </div>
   )
 }
