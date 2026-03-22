9444eda497a4275a9ace3b5548027509a2be8747
BraedonHaensel <bhaensel111@gmail.com>
2026-03-21 22:44:56 -0600

    style: switch to svh for better support of mobile browser overlays

--- apps/web/src/app/(app)/layout.tsx
+++ apps/web/src/app/(app)/layout.tsx
@@ -3,7 +3,7 @@ import MobileNavbar from '@/components/mobile-navbar'
 
 export default function AppLayout({ children }: { children: React.ReactNode }) {
   return (
-    <div className="flex h-screen min-w-[320px] flex-col">
+    <div className="flex h-svh min-w-[320px] flex-col">
       <DesktopNavbar className="hidden h-16 md:block" />
       <main className="bg-off-white flex flex-1 flex-col overflow-auto">
         {children}
