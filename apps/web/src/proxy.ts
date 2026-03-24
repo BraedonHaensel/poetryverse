e3f18d26dbbac47b4de600ff546a8e39491a6b10
BraedonHaensel <bhaensel111@gmail.com>
2026-03-23 21:05:06 -0600

    docs: add pseudocode for admin page protection

--- apps/web/src/proxy.ts
+++ apps/web/src/proxy.ts
@@ -7,12 +7,19 @@ export async function proxy(req: NextRequest) {
   const pathname = req.nextUrl.pathname
   const session = await getAuthSession()
 
-  if (session && !session.user.username) {
-    // Signed in without a username, redirect to the Login page
-    return NextResponse.redirect(new URL('/', req.url))
-  }
-
+  // Check if the user is signed in
   if (session) {
+    if (!session.user.username) {
+      // Signed in without a username, redirect to the Login page
+      return NextResponse.redirect(new URL('/', req.url))
+    }
+
+    // TODO require an admin user for the admin pages. Pseudocode:
+    // if (pathname.startsWith('/admin') && <is_not_an_admin>) {
+    //   // User must be an admin to access the admin pages
+    //   return NextResponse.redirect(new URL('/', req.url))
+    // }
+
     // Signed in, proceed to the page
     return NextResponse.next()
   }
@@ -21,8 +28,6 @@ export async function proxy(req: NextRequest) {
     // Guest mode allowed, proceed to the page
     return NextResponse.next()
   }
-
-  // TODO require an admin user for the admin pages
 
   // Sign in required, redirect to the Login page
   return NextResponse.redirect(new URL('/', req.url))
