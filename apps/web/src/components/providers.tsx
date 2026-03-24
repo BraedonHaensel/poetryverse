b695c7882c99fb2541d6b1e78cfafe64e6d828fc
BraedonHaensel <bhaensel111@gmail.com>
2026-03-23 20:45:12 -0600

    feat: add loading indicator until the auth session has been loaded

--- apps/web/src/components/providers.tsx
+++ apps/web/src/components/providers.tsx
@@ -1,8 +1,28 @@
 'use client'
-import { SessionProvider } from 'next-auth/react'
+
+import { LoaderCircle } from 'lucide-react'
+import { SessionProvider, useSession } from 'next-auth/react'
 import React from 'react'
 
 import { TooltipProvider } from './ui/tooltip'
+
+type SessionLoaderProps = {
+  children: React.ReactNode
+}
+
+/**
+ * Render a loading indicator until the session has been loaded.
+ */
+function SessionLoader({ children }: SessionLoaderProps) {
+  const { status } = useSession()
+
+  if (status === 'loading') {
+    return <LoaderCircle className="mx-auto mt-5 h-10 w-10 animate-spin" />
+  }
+
+  // Session loaded, display the page contents
+  return <>{children}</>
+}
 
 type Props = {
   children: React.ReactNode
@@ -11,7 +31,9 @@ type Props = {
 function Provider({ children }: Props) {
   return (
     <SessionProvider>
-      <TooltipProvider>{children}</TooltipProvider>
+      <SessionLoader>
+        <TooltipProvider>{children}</TooltipProvider>
+      </SessionLoader>
     </SessionProvider>
   )
 }
