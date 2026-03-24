6b158171cc271801ddd6111ce11584b6f1124878
BraedonHaensel <bhaensel111@gmail.com>
2026-03-24 12:30:51 -0600

    feat: add no settings for guest users notice

--- apps/web/src/app/(app)/settings/page.tsx
+++ apps/web/src/app/(app)/settings/page.tsx
@@ -5,6 +5,8 @@ import MobilePageHeader from '@/components/mobile-page-header'
 import { ShadowCard } from '@/components/shadow-card'
 import { Button } from '@/components/ui/button'
 import { CardContent, CardHeader, CardTitle } from '@/components/ui/card'
+import { getAuthSession } from '@/lib/nextauth'
+import { cn } from '@/lib/utils'
 
 import { AdvancedSettingsForm } from './forms/advanced-settings-form'
 import { EmailForm } from './forms/email-form'
@@ -37,30 +39,51 @@ function UserSettingsForms() {
 /**
  * User settings page.
  */
-export default function UserSettings() {
+export default async function UserSettings() {
+  const session = await getAuthSession()
+  const isGuest = !session
+
   return (
     <>
       {/* Mobile layout */}
       <div className="flex flex-1 flex-col md:hidden">
         <MobilePageHeader title="User Settings" />
         <div className="flex flex-1 flex-col gap-2 p-4">
-          <UserSettingsForms />
-          {/* Mobile-only sign out button */}
-          <SignOutButton className="mt-auto" />
+          {isGuest ? (
+            <ShadowCard className="p-2 md:m-auto">
+              <CardContent className="text-center">
+                There are no settings available for guest users.
+              </CardContent>
+            </ShadowCard>
+          ) : (
+            <>
+              <UserSettingsForms />
+              {/* Mobile-only sign out button */}
+              <SignOutButton className="mt-auto" />
+            </>
+          )}
         </div>
       </div>
 
       {/* Desktop layout */}
       <div className="m-auto hidden w-full p-10 md:block">
-        <ShadowCard className="m-auto max-w-170">
-          <CardHeader>
-            <CardTitle className="mx-auto text-2xl font-bold">
-              User Settings
-            </CardTitle>
-          </CardHeader>
-          <CardContent className="flex h-full flex-col gap-5">
-            <UserSettingsForms />
-          </CardContent>
+        <ShadowCard className={cn('m-auto max-w-170')}>
+          {isGuest ? (
+            <CardContent className="text-center">
+              There are no settings available for guest users.
+            </CardContent>
+          ) : (
+            <>
+              <CardHeader>
+                <CardTitle className="mx-auto text-2xl font-bold">
+                  User Settings
+                </CardTitle>
+              </CardHeader>
+              <CardContent className="flex h-full flex-col gap-5">
+                <UserSettingsForms />
+              </CardContent>
+            </>
+          )}
         </ShadowCard>
       </div>
     </>
