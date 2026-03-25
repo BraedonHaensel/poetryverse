e09dc9978a75ff22f99eca9adae318472123237e
BraedonHaensel <bhaensel111@gmail.com>
2026-03-25 12:49:41 -0600

    chore: update user, poem and like seeding

--- packages/database/prisma/seeding-data/poemLike.js
+++ packages/database/prisma/seeding-data/poemLike.js
@@ -1,32 +1,20 @@
-const poemLike = {
-  poemLike1: {
-    poemId1: 'cmn5hpjpr000304kzeqt67a4g',
-    userId1: 'cmn5grryw0000m68o3m0jttjo',
-  },
-  poemLike2: {
-    poemId2: 'cmn5hpjpr000304kzeqt67a4g',
-    userId2: 'cmn5gss8a0002m68oamdbn850',
-  },
-  poemLike3: {
-    poemId3: 'cmn5hpdln000204kzfbg941te',
-    userId3: 'cmn2apa7m0000wbrmgb2s775x',
-  },
-}
+import { poemIds } from './poem.js'
+import { userIds } from './user.js'
 
 export const poemLikeData = [
   {
-    poemId: poemLike.poemLike1.poemId1,
-    userId: poemLike.poemLike1.userId1,
-    createdAt: new Date('2026-03-26T03:09:16.151Z'),
+    poemId: poemIds.id1,
+    userId: userIds.user2,
+    createdAt: new Date('2026-03-21T03:09:16.151Z'),
   },
   {
-    poemId: poemLike.poemLike2.poemId2,
-    userId: poemLike.poemLike2.userId2,
-    createdAt: new Date('2026-03-27T07:12:16.151Z'),
+    poemId: poemIds.id1,
+    userId: userIds.user3,
+    createdAt: new Date('2026-03-22T07:12:16.151Z'),
   },
   {
-    poemId: poemLike.poemLike3.poemId3,
-    userId: poemLike.poemLike3.userId3,
-    createdAt: new Date('2026-03-28T03:12:13.151Z'),
+    poemId: poemIds.id3,
+    userId: userIds.user1,
+    createdAt: new Date('2026-03-23T03:12:13.151Z'),
   },
 ]
