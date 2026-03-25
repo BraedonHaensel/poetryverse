c783c42ef5a6eef1750afce5c40849584e687f90
chantaeh <chantae.ho@ucalgary.ca>
2026-03-24 22:19:34 -0600

    fix: remove export

--- packages/database/src/index.ts
+++ packages/database/src/index.ts
@@ -9,7 +9,6 @@ const adapter = new PrismaPg({ connectionString: process.env.DATABASE_URL })
 const logLevels = ['query' as const]
 
 export let prisma: PrismaClient
-export { Prisma }
 
 if (process.env.NODE_ENV === 'production') {
   prisma = new PrismaClient({ adapter })
