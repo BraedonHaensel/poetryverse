3a61ba87f5ebedf2bb26ed9577ba3f5c18670887
Carson May <carson.may@ucalgary.ca>
2026-03-27 15:50:40 -0600

    chore: change types to poem-types

--- apps/api/src/routes/api-router.ts
+++ apps/api/src/routes/api-router.ts
@@ -1,15 +1,15 @@
 import { Router } from 'express'
 
 import poemRoutes from './poem-routes'
+import poemTypeRoutes from './poem-type-routes'
 import reportRoutes from './report-routes'
 import tagRoutes from './tag-routes'
-import typeRoutes from './type-routes'
 import userRoutes from './user-routes'
 
 const router = Router()
 router.use('/users', userRoutes)
 router.use('/poems', poemRoutes)
-router.use('/types', typeRoutes)
+router.use('/poem-types', poemTypeRoutes)
 router.use('/tags', tagRoutes)
 router.use('/reports', reportRoutes)
 
--- apps/api/src/routes/poem-type-routes.ts
+++ apps/api/src/routes/poem-type-routes.ts
@@ -1,11 +1,11 @@
 import { Router } from 'express'
 
-import { getPoemTypes } from '../controllers/type-controller'
+import { getPoemTypes } from '../controllers/poem-type-controller'
 import { asyncHandler } from '../lib/async-handler'
 
 const router = Router()
 
-/** GET /api/types */
+/** GET /api/poem-types */
 router.get('/', asyncHandler(getPoemTypes))
 
 export default router
